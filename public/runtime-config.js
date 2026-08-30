f557f8b7e93ef83fac750b9d41abb8f449aca562
Benjamin Fortenberry <benfortenberry@outlook.com>
2026-08-30 12:34:01 -0500

    fix

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -3,7 +3,7 @@ window.__APP_CONFIG__ = Object.assign(
 		VITE_APP_API_URL: "https://accredi-track-production.up.railway.app",
 		VITE_AUTH0_DOMAIN: "",
 		VITE_AUTH0_CLIENT_ID: "",
-		VITE_AUTH0_AUDIENCE: "",
+		VITE_AUTH0_AUDIENCE: "https://accredi-track/api",
 	},
 	window.__APP_CONFIG__ || {}
 );
